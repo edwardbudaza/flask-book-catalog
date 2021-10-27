be4899d8854d5f0f0e5cece6845f5c7a064aa197
edwardbudaza <edward.budaza@yahoo.co.za>
2021-10-28 01:33:27 +0200

    advance flask app

---
+++ config/dev.py
@@ -0,0 +1,5 @@
+
+DEBUG = True
+SECRETE_KEY='topsecrete'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:pe86mfog09b5546@localhost/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
---
+++ config/prod.py
@@ -0,0 +1,4 @@
+DEBUG = False
+SECRETE_KEY='topsecrete'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:pe86mfog09b5546@localhost/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
