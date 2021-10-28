6d3890c2225f6c1a044ff605cefc843e5b42b178
edwardbudaza <edward.budaza@yahoo.co.za>
2021-10-28 14:35:13 +0200

    fixed errors sqlalchemy

--- config/prod.py
+++ config/prod.py
@@ -1,9 +1,12 @@
 import os
 import re
+import psycopg2
 
 DEBUG = False
 SECRETE_KEY='topsecrete'
 SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
 SQLALCHEMY_TRACK_MODIFICATIONS=False
