d850db65edf47b42a5811f0a09f71483239d6e52
edwardbudaza <edward.budaza@yahoo.co.za>
2021-10-28 02:45:33 +0200

    URI errors

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,9 @@
 import os
+import re
 
 DEBUG = False
 SECRETE_KEY='topsecrete'
 SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS=False
