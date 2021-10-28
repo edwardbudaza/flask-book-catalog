ecf0aa5381e99656f7dd219254ce07ec66d2f174
edwardbudaza <edward.budaza@yahoo.co.za>
2021-10-28 02:29:29 +0200

    error handling

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ flask_app = create_app('prod')
 
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry',email='harry@potters.com',password='secrete')
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry',email='harry@potters.com',password='secrete')
+    except:
         flask_app.run()
